d27e24c9ce8ef72eb8645965a53047e093a92e53
Forte <bryson.forte@wsu.edu>
2020-01-23 15:21:49 -0800

    screenshots

--- Lab2_StarterCode/FourToSixteenDecoder.sv
+++ Lab2_StarterCode/FourToSixteenDecoder.sv
@@ -23,13 +23,13 @@ module FourToSixteenDecoder(
 logic [0:3] EnTwoToFourDecoder;
 
 //prime decoder
-//TwoToFourDecoder TTFDPrime (.w(w[3:2]), .En(En), .y(EnTwoToFourDecoder[0:3]));		
+TwoToFourDecoder TTFDPrime (.w(w[3:2]), .En(En), .y(EnTwoToFourDecoder[0:3]));		
 
 // Design Logic
 TwoToFourDecoder TTFD0 (.w(w[1:0]), .En(EnTwoToFourDecoder[0]), .y(y[0:3]));
 
 //added more decoders
-//TwoToFourDecoder TTFD1 (.w(w[1:0]), .En(EnTwoToFourDecoder[1]), .y(y[4:7]));
-//TwoToFourDecoder TTFD2 (.w(w[1:0]), .En(EnTwoToFourDecoder[2]), .y(y[8:11]));
-//TwoToFourDecoder TTFD3 (.w(w[1:0]), .En(EnTwoToFourDecoder[3]), .y(y[12:15]));
+TwoToFourDecoder TTFD1 (.w(w[1:0]), .En(EnTwoToFourDecoder[1]), .y(y[4:7]));
+TwoToFourDecoder TTFD2 (.w(w[1:0]), .En(EnTwoToFourDecoder[2]), .y(y[8:11]));
+TwoToFourDecoder TTFD3 (.w(w[1:0]), .En(EnTwoToFourDecoder[3]), .y(y[12:15]));
 endmodule
