58add2dfd6f7744216821b2b305b736f07dd24a0
Gus <aengus.crowley@gmail.com>
2020-01-23 10:12:00 -0800

    attatched inputs to outputs

--- Lab2_StarterCode/FourToSixteenDecoder.sv
+++ Lab2_StarterCode/FourToSixteenDecoder.sv
@@ -19,17 +19,17 @@ module FourToSixteenDecoder(
 );
 
 // Declarations
+//does this need to be made into a wire? 
 logic [0:3] EnTwoToFourDecoder;
 
+//prime decoder
+//TwoToFourDecoder TTFDPrime (.w(w[3:2]), .En(En), .y(EnTwoToFourDecoder[0:3]));		
 
 // Design Logic
 TwoToFourDecoder TTFD0 (.w(w[1:0]), .En(EnTwoToFourDecoder[0]), .y(y[0:3]));
-// Replace this comment with four more instantiations of TwoToFourDecoder
 
-//prime decoder
-//TwoToFourDecoder TTFDPrime (.w(w[3:2]), .En(En), .y(y[0:3]));		Really not sure this is right
 //added more decoders
-//TwoToFourDecoder TTFD1 (.w(w[1:0]), .En(EnTwoToFourDecoder[1]), .y(y[0:3]));
-//TwoToFourDecoder TTFD2 (.w(w[1:0]), .En(EnTwoToFourDecoder[2]), .y(y[0:3]));
-//TwoToFourDecoder TTFD3 (.w(w[1:0]), .En(EnTwoToFourDecoder[3]), .y(y[0:3]));
+//TwoToFourDecoder TTFD1 (.w(w[1:0]), .En(EnTwoToFourDecoder[1]), .y(y[4:7]));
+//TwoToFourDecoder TTFD2 (.w(w[1:0]), .En(EnTwoToFourDecoder[2]), .y(y[8:11]));
+//TwoToFourDecoder TTFD3 (.w(w[1:0]), .En(EnTwoToFourDecoder[3]), .y(y[12:15]));
 endmodule
