83a78b24f391c4f3645ae44a34bb07957a34c1bc
Gus <aengus.crowley@gmail.com>
2020-01-30 15:35:21 -0800

    capitalization is a fuck

--- lab3/top_barrelshifter.sv
+++ lab3/top_barrelshifter.sv
@@ -1,12 +1,12 @@
 module top_barrel_shifter(
-	input logic [15:0] sw, //input data
+	input logic [15:0] SW, //input data
 	input logic BTNR, //BTNR
 	input logic BTNL, //BTNL
 	output logic [0:15] LED //output
 	);
 
 	barrel_shifter board_shifter (
-		.data(sw),
+		.data(SW),
 		.BTNR(BTNR),
 		.BTNL(BTNL),
 		.shout(LED)
