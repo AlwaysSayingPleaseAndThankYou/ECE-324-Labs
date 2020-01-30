5e29769d4d0f194846051c14431a330a320bf4f8
Gus <aengus.crowley@gmail.com>
2020-01-30 15:06:41 -0800

    formating

--- lab3/lab3.sv
+++ lab3/lab3.sv
@@ -9,10 +9,10 @@ module barrel_shifter(
 	 ? {data[14:0], data[15]}: data[15:0]);
 
 	//ec
-	assign shout[15:0] =
-	 (BTNR & BTNC) ? {data[1:0], data[15:2]} :
-	 (BTNL & BTNC) ? {data[13:0], data[15:14]}:
-	 BTNR ? {data[0], data[15:1]}:
-	 BTNL ? {data[14:0], data[15]};
+	//assign shout[15:0] =
+	 //(BTNR & BTNC) ? {data[1:0], data[15:2]} :
+	 //(BTNL & BTNC) ? {data[13:0], data[15:14]}:
+	 //BTNR ? {data[0], data[15:1]}:
+	 //BTNL ? {data[14:0], data[15]};
 
 endmodule : barrel_shifter 
