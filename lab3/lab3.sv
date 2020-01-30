041ee211dba7078230d67377a62c96a2da834e90
Gus <aengus.crowley@gmail.com>
2020-01-30 15:51:31 -0800

    forgot to add BTNC

--- lab3/lab3.sv
+++ lab3/lab3.sv
@@ -2,6 +2,7 @@ module barrel_shifter(
 	input logic [15:0] data,
 	input logic BTNR,
 	input logic BTNL,
+	input logic BTNC,
 	input logic FAKE
 	output logic [15:0] shout
 	);
@@ -10,10 +11,10 @@ module barrel_shifter(
 	 ? {data[14:0], data[15]}: data[15:0]);
 
 	//ec
-	//assign shout[15:0] =
-	 //(BTNR & BTNC) ? {data[1:0], data[15:2]} :
-	 //(BTNL & BTNC) ? {data[13:0], data[15:14]}:
-	 //BTNR ? {data[0], data[15:1]}:
-	 //BTNL ? {data[14:0], data[15]};
+	assign shout[15:0] =
+	 (BTNR & BTNC) ? {data[1:0], data[15:2]} :(
+	 (BTNL & BTNC) ? {data[13:0], data[15:14]}):
+	 (BTNR ? {data[0], data[15:1]}):
+	 BTNL ? {data[14:0], data[15]} : data[15:0];
 
 endmodule : barrel_shifter 
--- lab3/top_barrelshifter.sv
+++ lab3/top_barrelshifter.sv
@@ -2,11 +2,13 @@ module top_barrel_shifter(
 	input logic [15:0] SW, //input data
 	input logic BTNR, //BTNR
 	input logic BTNL, //BTNL
+	input logic BNTC,
 	output logic [0:15] LED //output
 	);
 
 	barrel_shifter board_shifter (
 		.data(SW),
+		.BTNC(BTNC),
 		.BTNR(BTNR),
 		.BTNL(BTNL),
 		.shout(LED)
