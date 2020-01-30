0de718b5502024fb1a7d75fce8178aa9d84a4252
Gus <aengus.crowley@gmail.com>
2020-01-30 15:04:14 -0800

    formatting and typos

--- lab3/lab3.sv
+++ lab3/lab3.sv
@@ -1,10 +1,18 @@
-module barrel_shifter(data, shift, shout);
-	input logic [15:0] data;
-	input logic BTNR;
-	input logic BTNL;
-	output logic [15:0] shout;
+module barrel_shifter(
+	input logic [15:0] data,
+	input logic BTNR,
+	input logic BTNL,
+	output logic [15:0] shout
+	);
 
-	assign shout[15:0] = BTNR ? {data[0], data[15:1]} : (BTNRL
-	 ? {data[14:0], data[15]}: data[15:0];)
+	assign shout[15:0] = BTNR ? {data[0], data[15:1]} : (BTNL
+	 ? {data[14:0], data[15]}: data[15:0]);
+
+	//ec
+	assign shout[15:0] =
+	 (BTNR & BTNC) ? {data[1:0], data[15:2]} :
+	 (BTNL & BTNC) ? {data[13:0], data[15:14]}:
+	 BTNR ? {data[0], data[15:1]}:
+	 BTNL ? {data[14:0], data[15]};
 
 endmodule : barrel_shifter 
