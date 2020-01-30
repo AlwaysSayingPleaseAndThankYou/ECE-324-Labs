e5cc0e4a1b46ee364299ea8b9c29961edfbc78ae
Gus <aengus.crowley@gmail.com>
2020-01-30 14:51:50 -0800

    added XDC, corrected assigns in top

--- lab3/lab3.sv
+++ lab3/lab3.sv
@@ -1,8 +1,10 @@
 module barrel_shifter(data, shift, shout);
 	input logic [15:0] data;
-	input logic direction;
+	input logic BTNR;
+	input logic BTNL;
 	output logic [15:0] shout;
 
-	assign shout[15:0] = direction ? {data[0], data[15:1]} : {data[14:0], data[15]};
+	assign shout[15:0] = BTNR ? {data[0], data[15:1]} : (BTNRL
+	 ? {data[14:0], data[15]}: data[15:0];)
 
 endmodule : barrel_shifter 
---
+++ lab3/top_barrelshifter.sv
@@ -0,0 +1,15 @@
+module top_barrel_shifter(
+	input logic [15:0] sw, //input data
+	input logic BTNR, //BTNR
+	input logic BTNL, //BTNL
+	output logic [0:15] LED //output
+	);
+
+	barrel_shifter board_shifter (
+		data(sw),
+		BTNR(BTNR),
+		BTNL(BTNL),
+		shout(LED)
+		);
+
+endmodule : top_barrel_shifter
