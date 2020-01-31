d81d2bc1ba545a0ddf1c46ce22c36a5ad5c6523b
Gus <aengus.crowley@gmail.com>
2020-01-30 16:03:42 -0800

    removed artifacts

--- lab3/lab3.sv
+++ lab3/lab3.sv
@@ -3,7 +3,6 @@ module barrel_shifter(
 	input logic BTNR,
 	input logic BTNL,
 	input logic BTNC,
-	input logic FAKE
 	output logic [15:0] shout
 	);
 
@@ -12,9 +11,9 @@ module barrel_shifter(
 
 	//ec
 	assign shout[15:0] =
-	 (BTNR & BTNC) ? {data[1:0], data[15:2]} :(
-	 (BTNL & BTNC) ? {data[13:0], data[15:14]}):
-	 (BTNR ? {data[0], data[15:1]}):
+	 (BTNR & BTNC) ? {data[1:0], data[15:2]} :
+	 (BTNL & BTNC) ? {data[13:0], data[15:14]}:
+	 BTNR ? {data[0], data[15:1]}:
 	 BTNL ? {data[14:0], data[15]} : data[15:0];
 
 endmodule : barrel_shifter 
